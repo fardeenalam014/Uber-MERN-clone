4b9b98a59eb1fd07b20870732862c750cb324250
Fardeen <fardeenalam0768@gmail.com>
2025-06-30 11:12:44 +0530

    first commit

---
+++ Backend/Database/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+function connectDB() {
+    mongoose.connect
+        (process.env.MONGO_URL.then(() => {
+            console.log("MongoDB connected successfully")
+        }).catch((err) => {
+            console.error("MongoDB connection failed:", err)
+
+        }))
+}
---
+++ Backend/Model/user.model.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+
+const userSchema = new mongoose.Schema({
+    fullname: {
+        firstname: {
+            type: String,
+            required: true,
+            minlength: [2, "First name must be at least 2 characters long"],
+            maxlength: [50, "First name must be at most 20 characters long"],
+        },
+        lastname: {
+            type: String,
+
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: [6, "Password must be at least 6 characters long"],
+        select:false, // Exclude password from queries by default
+    },
+    sockedId: {
+        type: String,
+        
+    },
+})
+
+userSchema.methods.generateAuthToken = function () {
+    const token = jwt.sign({ id: this._id }, process.env.JWT_SECRET);
+    return token;
+};
+userSchema.statics.hashpassword =async function(password){
+ return await bcrypt.hash(password,10);
+}
+userSchema.methods.comparepassword =async function(password){
+    return await bcrypt.compare(password, this.password);
+}
+const userModel =mongoose.model("UberUser",userSchema);
+
+export default userModel;
